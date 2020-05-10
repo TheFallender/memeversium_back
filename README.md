867d7001acb23b87674958b64e68d1f4abee8e7c
TheFallender <af.git@noxbit.net>
2020-05-10 02:52:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#  Memeversium Backend
+
+This repository seeks to host the backend for Memeversium.
+
+To run locally:
+1. Download the release and extract it.
+1. Run 'npm install'
+2. Run 'npm start'
+
+Memeversium frontend: https://github.com/TheFallender/memeversium
+Memeversium: https://thefallender.github.io/memeversium
---
+++ src/app.js
@@ -0,0 +1,88 @@
+//Libraries
+import {MongoClient} from 'mongodb';
+import 'babel-polyfill';
+
+//Resolvers
+import Query from "./resolvers/Query";
+import Mutation from "./resolvers/Mutation";
+import Meme from './resolvers/Meme'
+
+//TypeDefs
+import typeDefs from './schema';
+
+
+
+//ApolloServer
+	const {ApolloServer} = require('apollo-server');
+
+
+
+//MongoDB
+	//User info
+	const usr = "runAsAdmin";
+	const pwd = "Txg6FuaFA8g%5EN53Fxp";
+	const url = "thefallencluster-7uuhn.gcp.mongodb.net/test?authSource=admin&replicaSet=TheFallenCluster-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+
+//Connect to MongoDB
+const connectToDb = async function(usr, pwd, url) {
+	const uri = `mongodb+srv://${usr}:${pwd}@${url}`;
+	const client = new MongoClient(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+
+	await client.connect();
+	return client;
+};
+
+
+
+//Apollo Server
+const runApolloServer = function(context) {
+	//GraphQL
+		//Resolvers
+		const resolvers = {
+			Query,
+			Mutation,
+			Meme,
+		}
+
+	//Apollo Server
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context,
+	});
+
+	//Listen on URL
+	try {
+		server.listen().then(({url}) => {
+			console.log(`Server ready at ${url}`);
+		});
+	} catch (e) {
+		console.info(e);
+		server.close();
+	}
+}
+
+
+
+//Run the App
+const runApp = async function() {
+	const client = await connectToDb(usr, pwd, url);
+	console.log("Connecting to Mongo DB...");
+	try {
+		runApolloServer({
+			client,
+			db: client.db("memeversium"),
+			userCLT: client.db("memeversium").collection("users"),
+			memeCLT: client.db("memeversium").collection("memes")
+		});
+	} catch (e) {
+		console.log(e)
+		client.close();
+	}
+};
+
+//Execute the app
+runApp();
